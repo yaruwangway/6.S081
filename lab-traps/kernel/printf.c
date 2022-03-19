b647a2bc486048929ce3c4363f4d0850f81bf390
Soros Liu <soros.liu1029@gmail.com>
2022-03-19 17:29:21 +0800

    finish backtrace of lab traps

--- lab-traps/kernel/printf.c
+++ lab-traps/kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf(s);
   printf("\n");
   panicked = 1; // freeze uart output from other CPUs
+  backtrace();
   for(;;)
     ;
 }
@@ -131,4 +132,15 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void)
+{
+  struct proc *p = myproc();
+  uint64 fp = r_fp();
+  do {
+    printf("%p\n", *(uint64 *)(fp - 8));
+    fp = *(uint64 *)(fp - 16);
+  } while (PGROUNDDOWN(fp) != p->kstack+PGSIZE);
 }
